f0977dbea803dc2f441a7d673f475168c75f7c05
Abhilash R Jayakumar <62945844+abhlashrj@users.noreply.github.com>
2020-07-15 17:52:41 -0400

    Latest build changes

---
+++ spreadsheet/example/build/precache-manifest.b9ed7055d1a924215d4a69b5a645c2be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a947c1ae52f54a161d9b14636c3f1adb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41aa27f3ffd81bf017a9",
+    "url": "./static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "41aa27f3ffd81bf017a9",
+    "url": "./static/js/2.f0f88ac6.chunk.js"
+  },
+  {
+    "revision": "25b44fdcb97e8bc084e57e9e3d4ad514",
+    "url": "./static/js/2.f0f88ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb870a87b3c742c9e2d9",
+    "url": "./static/js/main.e42fcb72.chunk.js"
+  },
+  {
+    "revision": "5dbf7aa74148ffd4e16a",
+    "url": "./static/js/runtime-main.cb6cab9a.js"
+  }
+]);
