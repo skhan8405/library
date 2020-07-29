91750b63676785667d35e56cc91808649ddcfc82
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-07-29 06:56:51 -0400

    Row Delete Overlay CSS issue fix

--- grid/example/src/cells/DeletePopUpOverlay.js
+++ grid/example/src/cells/DeletePopUpOverlay.js
@@ -1,22 +1,14 @@
 import React, { memo } from "react";
 import ClickAwayListener from "react-click-away-listener";
 
-const divStyle = {
-    backgroundColor: "#ccc",
-    height: "200px",
-    width: "400px",
-    marginLeft: "-850px"
-};
-
 const DeletePopUpOverLay = memo(({ deleteRow, closeDeleteOverlay }) => {
     return (
         <ClickAwayListener onClickAway={closeDeleteOverlay}>
-            <div className="main-div-delete-overlay" style={divStyle}>
+            <div className="row-option-action-overlay delete">
                 <div className="cancel-save-buttons-delete">
                     <button className="delete-Button" onClick={deleteRow}>
                         Delete
                     </button>
-                    &nbsp;&nbsp;&nbsp;
                     <button className="cancel-Button" onClick={closeDeleteOverlay}>
                         Cancel
                     </button>
