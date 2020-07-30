6e7313034284ec5eb03cac9b963e6d78969c5e7c
djnadackal <djnadackal@gmail.com>
2020-07-30 15:59:52 +0530

    extDatagrid

--- spreadsheet/__tests__/src/common/extDataGrid.test.js
+++ spreadsheet/__tests__/src/common/extDataGrid.test.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
+import React from "react";
+import { shallow, mount } from "enzyme";
 import ExtDataGrid from "../../../src/common/extDataGrid";
 
-describe('<ExtDataGrid />', () => {
-    let rows = [
-        {name:'Shyam', age:24},
-        {name:'Abcd', age:25}
-    ]
-    const mockCellRangeSelection = jest.fn();
-    it('mount',  () => {
-        const wrapper = mount(
-            <ExtDataGrid 
-            columns={['name', 'age']}
-            rowsCount={rows.length}
-            rowGetter={(i) => rows[i]}
-            cellRangeSelection={mockCellRangeSelection}
-            />
-        );
-        expect(wrapper).not.toBeNull();
-        // TODO I am getting some error may be becasue I gave invalid properties. Please correct it.
-    
+describe("<ExtDataGrid />", () => {
+  let rows = [
+    { name: "Shyam", age: 24 },
+    { name: "Abcd", age: 25 },
+  ];
+  const mockCellRangeSelection = jest.fn();
+  it("mount", () => {
+    document.addEventListener[0] = jest.fn(() => {
+      document.getElementsByClassName("react-grid-Viewport")[0];
     });
+    const wrapper = mount(
+      <ExtDataGrid
+        columns={["name", "age"]}
+        rowsCount={rows.length}
+        rowGetter={(i) => rows[i]}
+        cellRangeSelection={mockCellRangeSelection}
+      />
+    );
+    wrapper.metricsUpdated = jest.fn();
+    expect(wrapper).not.toBeNull();
+    expect(wrapper.instance().metricsUpdated()).toBeNull();
+    // TODO I am getting some error may be becasue I gave invalid properties. Please correct it.
+    wrapper.unmount();
+  });
+  it("metricsUpdated function",()=>{
+      
+  })
 });
