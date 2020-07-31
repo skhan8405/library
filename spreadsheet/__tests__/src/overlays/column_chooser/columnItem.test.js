60041e5fb755c493d60e6aa902d343b410c124ba
djnadackal <djnadackal@gmail.com>
2020-07-31 18:13:54 +0530

    columnItem.test

--- spreadsheet/__tests__/src/overlays/column_chooser/columnItem.test.js
+++ spreadsheet/__tests__/src/overlays/column_chooser/columnItem.test.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
+import React from "react";
+import { shallow, mount } from "enzyme";
 import ColumnItem from "../../../../src/overlays/column_chooser/columnItem";
+import { DndProvider } from "react-dnd";
+import { TouchBackend } from "react-dnd-touch-backend";
 
-describe('<ColumnItem />', () => {
-    it('mount', () => {
-        const wrapper = mount(<ColumnItem/>)
-        expect(wrapper).not.toBeNull();
-    });
+
+
+
+describe("<ColumnItem />", () => {
+  const props = {
+    id: 1,
+    text: "text",
+    moveColumn: jest.fn(),
+    findColumn: jest.fn(() => 1),
+    useDrop: jest.fn(),
+  };
+  it("mount", () => {
+    const wrapper = mount(
+      <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+        <ColumnItem {...props} />
+      </DndProvider>
+    );
+
+    expect(wrapper.find("ColumnItem.useDrag.end")).not.toBeNull();
+    expect(wrapper.find("ColumnItem.useDrop.canDrop")).not.toBeNull();
+    expect(wrapper.find("div").length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
