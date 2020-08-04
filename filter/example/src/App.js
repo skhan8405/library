4bf495b9af4fb9a104351353bb2fc833b3d7dac6
djnadackal <djnadackal@gmail.com>
2020-08-04 22:49:04 +0530

    added comments in app.js

--- filter/example/src/App.js
+++ filter/example/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import Filter from "filter";
 import FilterData from "./data.json";
 export default function App() {
+   /**
+   * Following are the field arrays to be provided for the dropDown option
+   * Theses arrays with the key value pairs are recieved/mounted
+   */
   const airport = [
     { key: "AAA", value: "AAA" },
     { key: "AAB", value: "AAB" },
@@ -20,6 +24,9 @@ export default function App() {
     { key: "BAA", value: "BAA" },
     { key: "BBB", value: "BBB" },
   ];
+     /**
+   * This is where the recieved array of dropdown options are redefined with the passing filter object
+   */
   FilterData.filter.forEach((item) => {
     if (item.types) {
       for (let i in item.types) {
@@ -31,12 +38,24 @@ export default function App() {
       }
     }
   });
+   /**
+   * Method displaying the applied filters at that moment
+   * @param {*} appliedFilter is the specific applied filters array
+   */
   const appliedFilters = (appliedFilter) => {
     console.log("appliedFilter:", appliedFilter);
   };
+     /**
+   * Method displaying the saved filters at that moment
+   * @param {*} savedFilter is the specific saved filters array
+   */
   const savedFilters = (savedFilter) => {
     console.log("savedFilter:", savedFilter);
   };
+       /**
+   * Method displaying the favourite filters
+   * @param {*} favourite is the favourite filter array that just got added as favourite
+   */
   const addingToFavourite = (favourite) => {
     console.log(favourite);
   };
