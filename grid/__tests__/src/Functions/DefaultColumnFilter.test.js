ae62068741255200cf2e6da484a05f68792f3c88
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-08-13 16:35:54 -0400

    Increased the coverage percentage for DefaultColumnFilter.test.js

--- grid/__tests__/src/Functions/DefaultColumnFilter.test.js
+++ grid/__tests__/src/Functions/DefaultColumnFilter.test.js
@@ -1,18 +1,25 @@
 /* eslint-disable no-undef */
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import DefaultColumnFilter from "../../../src/Functions/DefaultColumnFilter";
 import "@testing-library/jest-dom/extend-expect";
 
 describe("DefaultColumnFilter component", () => {
-    const filterValue = "23";
-    const setFilter = jest.fn(() => "222");
-    const wrapper = render(
-        <DefaultColumnFilter column={(filterValue, setFilter)} />
-    );
-    const input = wrapper.getByPlaceholderText("Search");
+    it("should search the given value", () => {
+        const newSetFilter = jest.fn();
+        const newFilterValue = "XX2225";
 
-    it("Should check DefaultColumnFilter with onChange ", () => {
-        expect(input).toBeInTheDocument();
+        const { container } = render(
+            <DefaultColumnFilter
+                column={{
+                    filterValue: newFilterValue,
+                    setFilter: newSetFilter
+                }}
+            />
+        );
+
+        const input = container.getElementsByClassName("txt").item(0);
+        fireEvent.change(input, { target: { value: "ABC1178" } });
+        expect(newSetFilter).toHaveBeenCalledTimes(1);
     });
 });
