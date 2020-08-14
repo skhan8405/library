1d27b8f28c00eb6c960e47f85fe402fb2cb45c5e
djnadackal <djnadackal@gmail.com>
2020-08-14 11:15:19 +0530

    test updates for spreadsheet
    test updates for spreadsheet

--- spreadsheet/__tests__/src/overlays/sorting/SortingList.test.js
+++ spreadsheet/__tests__/src/overlays/sorting/SortingList.test.js
@@ -1,10 +1,5 @@
-import React from "react";
-import { act } from "react-dom/test-utils";
-import ReactTestUtils from "react-dom/test-utils";
-import SortingList from "../../../../src/overlays/sorting/SortingList";
-import { DndProvider } from "react-dnd";
-import { TouchBackend } from "react-dnd-touch-backend";
 import "@testing-library/jest-dom/extend-expect";
+/* eslint-disable no-undef */
 
 let container;
 
@@ -17,86 +12,3 @@ afterEach(() => {
     document.body.removeChild(container);
     container = null;
 });
-// const cards = [
-//   {
-//     id: 1,
-//     key: "flightno",
-//     text: "FlightNo",
-//   },
-//   {
-//     id: 2,
-//     key: "yield",
-//     text: "Yield",
-//   },
-//   {
-//     id: 3,
-//     key: "revenue",
-//     text: "Revenue",
-//   },
-// ];
-// const props = {
-//   sortsArray: [...cards],
-// };
-
-// it("mount", () => {
-//   const handleReorderList = (p) => {};
-
-//   const wrapper = mount(
-//     <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
-//       <SortingList
-//         props={props}
-//         sortsArray={cards}
-//         handleReorderListOfSort={handleReorderList}
-//       />
-//     </DndProvider>
-//   );
-//   expect(wrapper.find("SortItem")).not.toBeNull();
-// });
-
-// it("CardList item length", () => {
-//   let valuesUpdated = [];
-//   const handleReorderList = (values) => (valuesUpdated = [...values]);
-//   const wrapper = mount(
-//     <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
-//       <SortingList
-//         props={props}
-//         sortsArray={cards}
-//         handleReorderListOfSort={handleReorderList}
-//       />
-//     </DndProvider>
-//   );
-//   expect(wrapper.find("Card").length).toEqual(3);
-// });
-
-// it("CardList moveCard()", () => {
-//   let valuesUpdated = [];
-//   const handleReorderListOfSort = jest.fn();
-//   const onMoveCard = jest.fn();
-//   const onFindCard = jest.fn();
-//   let card = [];
-//   cards.forEach((a) =>
-//     card.push({ ...a, moveCard: onMoveCard, findCard: onFindCard })
-//   );
-//   props.sortsArray = card;
-
-//   act(() => {
-//     let wrapper = mount(
-//       <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
-//         <SortingList
-//           moveCard={onMoveCard}
-//           findCard={onFindCard}
-//           sortsArray={cards}
-//           handleReorderListOfSort={handleReorderListOfSort}
-//         />
-//       </DndProvider>
-//     );
-
-//     expect(wrapper.find("Card").at(0).props().id).toEqual("1");
-
-//     wrapper.find("Card").at(0).props().moveCard(1, 2);
-//   });
-
-//   expect(onMoveColumn).toHaveBeenCalledTimes(1);
-
-//   expect(handleReorderList).toHaveBeenCalledTimes(1);
-// });
