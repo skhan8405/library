9c528fc0496ce178fa819ec77070e86edffd5b4c
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-08-14 00:18:01 -0400

    Increased the coverage percentage for ColumnsUtilities.test.js

--- grid/__tests__/src/Utilities/ColumnsUtilities.test.js
+++ grid/__tests__/src/Utilities/ColumnsUtilities.test.js
@@ -1,11 +1,69 @@
 /* eslint-disable no-undef */
+import React from "react";
 import {
     extractColumns,
     extractAdditionalColumn
 } from "../../../src/Utilities/ColumnsUtilities";
 
-const mockDisplayCell = jest.fn();
+const mockFlightDisplayCell = jest.fn((rowData, DisplayTag) => {
+    const { flightno, date } = rowData.flight;
+    return (
+        <div className="flight-details">
+            <DisplayTag cellKey="flightno" columnKey="flight">
+                <strong>{flightno}</strong>
+            </DisplayTag>
+            <DisplayTag columnKey="flight" cellKey="date">
+                <span>{date}</span>
+            </DisplayTag>
+        </div>
+    );
+});
+const mockFlightEditCell = jest.fn((rowData, DisplayTag, rowUpdateCallBack) => {
+    const { flightno, date } = rowData.flight;
+    return (
+        <div>
+            <DisplayTag columnKey="flight" cellKey="flightno">
+                <input
+                    data-testid="flightnoinput"
+                    className="flight-no-input"
+                    type="text"
+                    value={flightno}
+                    onChange={() => rowUpdateCallBack("nothing")}
+                />
+            </DisplayTag>
+            <DisplayTag columnKey="flight" cellKey="date">
+                <input
+                    type="date"
+                    value={date}
+                    onChange={mockUpdateDateValue}
+                />
+            </DisplayTag>
+        </div>
+    );
+});
+const mockSegmentDisplayCell = jest.fn((rowData, DisplayTag) => {
+    const { from, to } = rowData.segment;
+    return (
+        <div className="segment-details">
+            <DisplayTag columnKey="segment" cellKey="from">
+                <span>{from}</span>
+            </DisplayTag>
+            <i>
+                <img src={FlightIcon} alt="segment" />
+            </i>
+            <DisplayTag columnKey="segment" cellKey="to">
+                <span>{to}</span>
+            </DisplayTag>
+        </div>
+    );
+});
 const columns = [
+    {
+        Header: "Id",
+        accessor: "travelId",
+        width: 50,
+        disableFilters: true
+    },
     {
         Header: "Flight",
         accessor: "flight",
@@ -21,11 +79,81 @@ const columns = [
             }
         ],
         sortValue: "flightno",
-        displayCell: mockDisplayCell,
-        columnId: "column_0",
-        displayInExpandedRegion: false
+        displayCell: mockFlightDisplayCell,
+        editCell: mockFlightEditCell
+    },
+    {
+        Header: "Segment",
+        accessor: "segment",
+        width: 100,
+        onlyInDesktop: true,
+        innerCells: [
+            {
+                Header: "From",
+                accessor: "from"
+            },
+            {
+                Header: "To",
+                accessor: "to"
+            }
+        ],
+        displayCell: mockSegmentDisplayCell
     }
 ];
+const additionalColumn = {
+    Header: "Remarks",
+    innerCells: [
+        { Header: "Remarks", accessor: "remarks" },
+        { Header: "Details", onlyInTablet: true, accessor: "details" }
+    ],
+    displayCell: (rowData, DisplayTag) => {
+        const { remarks, details } = rowData;
+        const {
+            startTime,
+            endTime,
+            status,
+            additionalStatus,
+            flightModel,
+            bodyType,
+            type,
+            timeStatus
+        } = details;
+        return (
+            <div className="details-wrap">
+                <DisplayTag columnKey="remarks" cellKey="remarks">
+                    <ul>
+                        <li>{remarks}</li>
+                    </ul>
+                </DisplayTag>
+                <DisplayTag columnKey="details" cellKey="details">
+                    <ul>
+                        <li>
+                            {startTime} - {endTime}
+                        </li>
+                        <li className="divider">|</li>
+                        <li>
+                            <span>{status}</span>
+                        </li>
+                        <li className="divider">|</li>
+                        <li>{additionalStatus}</li>
+                        <li className="divider">|</li>
+                        <li>{flightModel}</li>
+                        <li className="divider">|</li>
+                        <li>{bodyType}</li>
+                        <li className="divider">|</li>
+                        <li>
+                            <span>{type}</span>
+                        </li>
+                        <li className="divider">|</li>
+                        <li>
+                            <span>{timeStatus}</span>
+                        </li>
+                    </ul>
+                </DisplayTag>
+            </div>
+        );
+    }
+};
 const searchColumn = jest.fn();
 const isDesktop = false;
 const updateRowInGrid = jest.fn();
@@ -39,6 +167,6 @@ test("Test extractAdditionalColumn", () => {
     const mockExtractColumns = jest
         .fn()
         .mockImplementation(extractAdditionalColumn);
-    mockExtractColumns(columns, isDesktop);
+    mockExtractColumns(additionalColumn, isDesktop);
     expect(mockExtractColumns).toHaveBeenCalled();
 });
