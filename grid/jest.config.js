0e19fe8851fe4fd3ea6846df9242fda27a15a408
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-08-17 12:23:49 -0400

    Es lint formatting

--- grid/jest.config.js
+++ grid/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  projects: ["<rootDir>/setupTest.js"],
-  coverageReporters: ["text", "lcov", "json", "text", "clover", "cobertura"],
-  collectCoverage: true
+    projects: ["<rootDir>/setupTest.js"],
+    coverageReporters: ["text", "lcov", "json", "text", "clover", "cobertura"],
+    collectCoverage: true
 };
