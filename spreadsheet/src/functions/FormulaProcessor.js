6b2595b34d7998d8a86c0aaae3eb15d085539859
djnadackal <djnadackal@gmail.com>
2020-08-25 17:19:04 +0530

    Drag calculation
    implementation + test cases

--- spreadsheet/src/functions/FormulaProcessor.js
+++ spreadsheet/src/functions/FormulaProcessor.js
@@ -1,38 +1,74 @@
 export default function FormulaProcessor(expression) {
+    let calculationObject = {
+        operation: "",
+        columnArray: []
+    };
     let columnArray = [];
-    if (expression.match(/^=sum\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/g)) {
+    if (
+        expression.match(
+            /^=(sum|mul|diff|add|prod|sub|max|min)\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i
+        )
+    ) {
         console.log(
-            expression.match(/^=sum\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/g)
+            expression.match(
+                /^=(sum|mul|diff|add|prod|sub|max|min)\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i
+            )
         );
-        const RegCode = /^=sum\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$$/g;
+        const RegCode = /^=(sum|mul|diff|add|prod|sub|max|min)\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i;
         const exp = RegCode.exec(expression);
         const parameter1 = exp.groups.one;
         const parameter2 = exp.groups.more;
         if (parameter1.match(/^(c\d*?)$/i)) {
-            const RegCode1 = /^c(?<column>\d*?)$/g;
+            const RegCode1 = /^c(?<column>\d*?)$/i;
             const exper1 = RegCode1.exec(parameter1);
             columnArray.push(Number(exper1.groups.column));
         }
-        if (parameter2.length > 1) {
+        if (parameter2.length > 3) {
             const moreParameters = parameter2.split(",");
             moreParameters.forEach((item) => {
                 if (item.match(/^(c\d*?)$/i)) {
-                    const RegCodes = /^c(?<column>\d*?)$/g;
+                    const RegCodes = /^c(?<column>\d*?)$/i;
                     const expers = RegCodes.exec(item);
                     columnArray.push(Number(expers.groups.column));
                 }
             });
-        } else {
-            if (parameter2.match(/^(c\d*?)$/i)) {
-                const RegCode2 = /^c(?<column>\d*?)$/g;
-                const exper2 = RegCode2.exec(parameter2);
-                columnArray.push(Number(exper2.groups.column));
-            }
+        } else if (parameter2.match(/^(c\d*?)$/i)) {
+            const RegCode2 = /^c(?<column>\d*?)$/i;
+            const exper2 = RegCode2.exec(parameter2);
+            columnArray.push(Number(exper2.groups.column));
         }
     }
 
     if (columnArray.length > 1) {
-        console.log(columnArray);
-        return columnArray;
+        if (
+            expression.match(
+                /^=(sum|add)\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i
+            )
+        ) {
+            calculationObject.operation = "SUM";
+        } else if (
+            expression.match(
+                /^=(diff|sub)\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i
+            )
+        ) {
+            calculationObject.operation = "DIFF";
+        } else if (
+            expression.match(
+                /^=(mul|prod)\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i
+            )
+        ) {
+            calculationObject.operation = "MUL";
+        } else if (
+            expression.match(/^=max\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i)
+        ) {
+            calculationObject.operation = "MAX";
+        } else if (
+            expression.match(/^=min\((?<one>.\w*?),(?<more>(.\w*?)*?)\)$/i)
+        ) {
+            calculationObject.operation = "MIN";
+        }
+        calculationObject.columnArray = [...columnArray];
+        console.log(calculationObject);
+        return calculationObject;
     } else return [];
 }
